1618d0dcffd895332d4900e4de162c450d0d9255
ashish-greycube <mr.ashish.shah@gmail.com>
2021-07-08 12:31:23 +0530

    feat: usage-info link in help menu

--- limit/hooks.py
+++ limit/hooks.py
@@ -16,7 +16,7 @@ app_license = "MIT"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/limit/css/limit.css"
-# app_include_js = "/assets/limit/js/limit.js"
+app_include_js = "/assets/limit/js/limit.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/limit/css/limit.css"
---
+++ limit/public/js/limit.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+  $('.dropdown-menu-right#toolbar-help').append('<a class="dropdown-item" href="/app/usage-info">           Dashboard Usage          </a>')
+});
