6a22c308a9ebaeb6dabb13c3ebc6a724833658ad
ashish-greycube <mr.ashish.shah@gmail.com>
2021-07-16 11:30:13 +0530

    fix: put timout for usuage link

--- limit/public/js/limit.js
+++ limit/public/js/limit.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
+
   // v13 link
-  $('.dropdown-menu-right#toolbar-user').append('<div class="dropdown-divider"></div><a class="dropdown-item" href="/app/usage-info">           Dashboard Usage          </a>')
+  setTimeout(() => {
+    $('.dropdown-menu-right#toolbar-user').append('<div class="dropdown-divider"></div><a class="dropdown-item" href="/app/usage-info">           Dashboard Usage          </a>')
+  }, 200);
+
   // v12 link
-  $('#toolbar-user').append('<li class="divider"></li><li><a href="/desk#usage-info">       Dashboard Usage</a></li>')
+  setTimeout(() => {
+    $('#toolbar-user').append('<li class="divider"></li><li><a href="/desk#usage-info">       Dashboard Usage</a></li>')
+  }, 200);
+  
 });
