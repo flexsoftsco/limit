6cf0ec7d97137b216c9307b1b5768421573eb666
ashish-greycube <mr.ashish.shah@gmail.com>
2021-10-27 09:42:35 +0530

    fix: usuage info twice remove

--- limit/public/js/limit.js
+++ limit/public/js/limit.js
@@ -1,7 +1,7 @@
 $(document).on('toolbar_setup',function(){
   // v13 link
-  $('.dropdown-menu-right#toolbar-user').append('<div class="dropdown-divider"></div><a class="dropdown-item" href="/app/usage-info">           Usage Info          </a>')
+  $('div.dropdown-menu-right#toolbar-user').append('<div class="dropdown-divider"></div><a class="dropdown-item" href="/app/usage-info">           Usage Info          </a>')
 
   // v12 link
-  $('#toolbar-user').append('<li class="divider"></li><li><a href="/desk#usage-info">       Usage Info</a></li>')
+  $('ul#toolbar-user').append('<li class="divider"></li><li><a href="/desk#usage-info">       Usage Info</a></li>')
 });
